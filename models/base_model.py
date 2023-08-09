c3d9a8d454411f57cee4d5df15cde5b1fc6f5b8d
njdam <jeandamngaboyimana@gmail.com>
2023-08-09 07:32:26 +0000

    Create BaseModel from dictionary.

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,20 @@ class BaseModel:
     A class BaseModel where all model's classes inherited.
     """
 
-    def __init__(self):
+    def __init__(self, *arg, **kwargs):
         """
         Initiatisation of every BaseModal with it's unique Id.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,27 +1,68 @@
 #!/usr/bin/python3
 
+import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
 
-class TestBaseModelClass(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
     """
-    A test cases for BaseModel class.
+    A test cases for BaseModel Class.
     """
-    def test_save(self):
-        """
-        A test case for saving.
-        """
+
+    def test_attributes_existence(self):
         my_model = BaseModel()
-        my_model.name = "My First Model"
-        my_model.my_number = 89
-        print(my_model)
+        self.assertTrue(hasattr(my_model, 'id'))
+        self.assertTrue(hasattr(my_model, 'created_at'))
+        self.assertTrue(hasattr(my_model, 'updated_at'))
+
+    def test_id_generation(self):
+        my_model1 = BaseModel()
+        my_model2 = BaseModel()
+        self.assertNotEqual(my_model1.id, my_model2.id)
+
+    def test_created_at(self):
+        my_model = BaseModel()
+        self.assertIsInstance(my_model.created_at, datetime)
+
+    def test_updated_at(self):
+        my_model = BaseModel()
+        self.assertIsInstance(my_model.updated_at, datetime)
+
+    def test_str_method(self):
+        my_model = BaseModel()
+        str_representation = "[BaseModel] ({}) {}".format(my_model.id, my_model.__dict__)
+        self.assertEqual(str(my_model), str_representation)
+
+    def test_save_method(self):
+        my_model = BaseModel()
+        original_updated_at = my_model.updated_at
         my_model.save()
-        print(my_model)
-        my_model_json = my_model.to_dict()
-        print(my_model_json)
-        print("JSON of my_model:")
-        for key in my_model_json.keys():
-            print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+        self.assertNotEqual(original_updated_at, my_model.updated_at)
+
+    def test_to_dict_method(self):
+        my_model = BaseModel()
+        my_model_dict = my_model.to_dict()
+        self.assertIsInstance(my_model_dict, dict)
+        self.assertEqual(my_model_dict['__class__'], 'BaseModel')
+        self.assertIsInstance(my_model_dict['created_at'], str)
+        self.assertIsInstance(my_model_dict['updated_at'], str)
+        self.assertEqual(my_model_dict['id'], my_model.id)
+
+    def test_to_dict_with_kwargs(self):
+        kwargs = {
+            'id': 'test-id',
+            'created_at': '2023-08-08T12:00:00.000000',
+            'updated_at': '2023-08-08T12:30:00.000000',
+            'name': 'Test Model'
+        }
+        my_model = BaseModel(**kwargs)
+        my_model_dict = my_model.to_dict()
+        self.assertEqual(my_model_dict['__class__'], 'BaseModel')
+        self.assertEqual(my_model_dict['id'], 'test-id')
+        self.assertEqual(my_model_dict['created_at'], '2023-08-08T12:00:00.000000')
+        self.assertEqual(my_model_dict['updated_at'], '2023-08-08T12:30:00.000000')
+        self.assertEqual(my_model_dict['name'], 'Test Model')
 
 
 if __name__ == '__main__':
