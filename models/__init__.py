32d80236a534d90d63e0b6007e4d79ada8baeea6
njdam <jeandamngaboyimana@gmail.com>
2023-08-13 18:55:44 +0000

    Updated files

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
-# The __init__ magic method for models dictionary to get storage path
+# The magic method for essential linking information between files
 
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 storage = FileStorage()
 storage.reload()
+
+
+classes = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review
+        }
