50782fd2e8d6e8209786d400e0f090c782cc2d5c
njdam <jeandamngaboyimana@gmail.com>
2023-08-13 17:04:01 +0000

    A class Amenity creation.

---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# A file that bear class Amenity and inherited from class BaseModel.
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """A class Amenity that inherited from BaseModel class.
+    A public class attribute:
+        name: string - empty string.
+    """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialisation of class Amenity."""
+        super().__init__(*args, **kwargs)
