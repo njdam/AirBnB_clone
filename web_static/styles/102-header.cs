66c9351e1b19375e7cef71b3ab5ea13262d99cca
njdam <jeandamngaboyimana@gmail.com>
2023-08-22 00:17:14 +0000

    Updated.

---
+++ web_static/styles/102-header.cs
@@ -0,0 +1,10 @@
+header {
+	background-color: white;
+	height: 70px;
+	width: 100%;
+	border-bottom: 1px solid #CCCCCC;
+	background-image: url(../images/logo.png);
+	background-repeat: no-repeat; /* No repeat of Image */
+	/* At x: 20px, at y: 0 */
+	background-position: 20px 0; /* or `background-position-x: 20px`*/
+}
